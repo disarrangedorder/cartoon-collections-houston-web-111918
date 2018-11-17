3440d20d25d4895bf14853146a386593fecf83b3
Claire Nguyen <ms.clairenguyen@gmail.com>
2018-11-17 22:07:05 +0000

    automatically backed up by learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -19,8 +19,8 @@ def long_planeteer_calls(array_of_calls)# code an argument here
   array_of_calls.any? { |calls|
     calls.length > 4
   }
-  
-  
+
+
 end
 
 def find_the_cheese# code an argument here
