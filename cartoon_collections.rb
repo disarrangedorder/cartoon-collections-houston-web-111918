c45b6590d24cbe9c7697239143e7f96fbe11add1
Claire Nguyen <ms.clairenguyen@gmail.com>
2018-11-17 23:07:44 +0000

    automatically backed up by learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -21,10 +21,10 @@ def long_planeteer_calls(array_of_calls)# code an argument here
   }
 end
 
-def find_the_cheese(maybe_chessy)# code an argument here
+def find_the_cheese(foods)# code an argument here
   # the array below is here to help
   cheese_types = ["cheddar", "gouda", "camembert"]
-  maybe_cheesy.find {|items| items.include?(cheese_types)}
+  foods.find {|food| food.include?(cheese_types)}
 
 
 end
